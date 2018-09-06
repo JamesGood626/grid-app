872ce3f205509be0d4247dffdbd46f086f7abf0f
James <jamesgood626@gmail.com>
2018-09-06 10:09:10 -0700

    Successfully adding children, grand-children, and grand-grand-children with updateHtmlElement redux action. Wooo.

--- src/state/__tests__/reducers.test.js
+++ src/state/__tests__/reducers.test.js
@@ -27,7 +27,23 @@ const childElPayload = {
   // and determine the parent
   // This id is necessary for UPDATE_HTML_ELEMENT,
   // So that we know which byId object to update.
-  parent_id: 'second-element',
+  parent_id_arr: ['second-element'],
+  element_type: 'span',
+  children_list: [],
+  all_children_ids: [],
+}
+
+const grandChildElPayload = {
+  id: 'grand-child-element',
+  parent_id_arr: ['second-element', 'child-element'],
+  element_type: 'div',
+  children_list: [],
+  all_children_ids: [],
+}
+
+const grandGrandChildElPayload = {
+  id: 'grand-grand-child-element',
+  parent_id_arr: ['second-element', 'child-element', 'grand-child-element'],
   element_type: 'h1',
   children_list: [],
   all_children_ids: [],
@@ -64,6 +80,30 @@ describe('Reducer', () => {
     store.dispatch(updateHtmlElement(childElPayload))
     const state = store.getState()
     console.log('THIS IS THE STATE RESULT OF ADDING CHILD: ', state.html.byId)
+    // expect(state.html.byId[payload.id]).toEqual(payload)
+    // expect(state.html.byId[payloadTwo.id]).toEqual(payloadTwo)
+    // expect(state.html.allIds.length).toBe(2)
+    // expect(state.html.allIds[0]).toBe(payload.id)
+    // expect(state.html.allIds[1]).toBe(payloadTwo.id)
+  })
+
+  test('adds grandchild html element to parent element state', () => {
+    store.dispatch(addHtmlElement(payloadTwo))
+    store.dispatch(updateHtmlElement(childElPayload))
+    store.dispatch(updateHtmlElement(grandChildElPayload))
+    store.dispatch(updateHtmlElement(grandGrandChildElPayload))
+    const state = store.getState()
+    console.log(
+      'THIS IS THE STATE RESULT OF ADDING GRANDCHILD: ',
+      state.html.byId
+    )
+    console.log(
+      state.html.byId['second-element'].children_list_byId['child-element']
+    )
+    console.log(
+      state.html.byId['second-element'].children_list_byId['child-element']
+        .children_list_byId['grand-child-element']
+    )
     // expect(state.html.byId[payload.id]).toEqual(payload)
     // expect(state.html.byId[payloadTwo.id]).toEqual(payloadTwo)
     // expect(state.html.allIds.length).toBe(2)
--- src/state/reducers/htmlReducer.js
+++ src/state/reducers/htmlReducer.js
@@ -15,12 +15,21 @@ import { initialState } from '../index'
 // interface IHtmlElement {
 //  id: string
 //  element_type: string
-//  children_list: IHtmlElement[]
+//  children_list_byId: IHtmlElement {}
 //  all_children_ids: string[id]
 // }
 
 // inside of allIds: string[]
 // IHtmlElement id
+
+const reverseArray = arr => {
+  const length = arr.length
+  const reversedArray = []
+  for (let i = length - 1; i >= 0; i--) {
+    reversedArray.push(arr[i])
+  }
+  return reversedArray
+}
 
 // The current implementation of add may need to be changed to be UPDATE_HTML_ELEMENTS
 export default function(state = initialState, action) {
@@ -39,31 +48,55 @@ export default function(state = initialState, action) {
     case UPDATE_HTML_ELEMENT:
       const { parent_id } = action.payload
       const iterateForUpdate = (state, action, parent_id) => {
-        const parentStateObj = state.byId[parent_id]
-        if (parentStateObj !== 'undefined') {
-          console.log('THIS SHOULD WORK')
-          return {
-            ...state,
-            byId: {
-              ...state.byId,
-              [parent_id]: {
-                ...parentStateObj,
-                children_list: [
-                  ...parentStateObj.children_list,
-                  action.payload,
-                ],
-                all_children_ids: [
-                  ...parentStateObj.all_children_ids,
-                  action.payload.id,
-                ],
-              },
-            },
-          }
-        }
+        // And honestly, if I utilize a parent_id array (Which I pretty much have to)
+        // then doing this won't even be necessary, because I'll already know in advance
+        // how many levels I'll need to traverse, so I can keep a counter which will
+        // be a parameter to the recurse function.
+        // The parent_id array will be maintained in local component state
+        // and the stack will be added/removed from as the user clicks forward/back
+        // through the HTML element levels. So, that'll be easy to pass into the
+        // dispatch function for updateHtmlElement.
+        //*******Works but will prolly replace */
+        // const parentStateObj = state.byId[parent_id]
+        // if (parentStateObj !== 'undefined') {
+        //   console.log('THIS SHOULD WORK')
+        //   return {
+        //     ...state,
+        //     byId: {
+        //       ...state.byId,
+        //       [parent_id]: {
+        //         ...parentStateObj,
+        //         children_list: [
+        //           ...parentStateObj.children_list,
+        //           action.payload,
+        //         ],
+        //         all_children_ids: [
+        //           ...parentStateObj.all_children_ids,
+        //           action.payload.id,
+        //         ],
+        //       },
+        //     },
+        //   }
+        // }
+        // ************************************/
         // If it's not in the first object, then you'll need to
         // check the children_list, and see if it's in that byId object.
         // And so, on and so forth, until a reference is obtained
-
+        const stack = []
+        console.log('THE ACTION.PAYLOAD: ', action.payload)
+        const firstParent = action.payload.parent_id_arr[0]
+        const htmlElementObj = state.byId[firstParent]
+        stack.push(htmlElementObj)
+        console.log('THE STACK AFTER FIRST PUSH: ', stack)
+        const count = 1
+        return recurseToFindTargetHtmlById(
+          state,
+          action.payload,
+          action.payload.parent_id_arr,
+          count,
+          stack,
+          htmlElementObj
+        )
         // It would be easiest, to utilize a recursive function.
         // Which maintains the stack (necessary for keeping track of the parent objects
         // for updating byId after the target element is updated with the new children)
@@ -74,17 +107,87 @@ export default function(state = initialState, action) {
       }
       const recurseToFindTargetHtmlById = (
         state,
-        action,
-        parent_id,
+        payload,
+        parent_id_arr,
+        count,
         stack,
         htmlElementObj
       ) => {
-        if (htmlElementObj.byId[parent_id]) {
-          // Do the update to the children_list and all_children_ids
-          // Then begin the unraveling of the stack by utilizing a decrementing
-          // for loop to iterate and update each preceding parent object
-          // and finally return.
+        // Using count(starting from 2) - 1 to access elements in the array, as I'll need to check
+        // against the array length to know when I've reached the target Html element's id.
+        // levelOfParentIds accomodates for the shifting the first el from parent_id_arr and adding
+        // it to the stack.
+        const levelOfParentIds = parent_id_arr.length
+        console.log('THE COUNT: ', count)
+        console.log('THIS IS THE LEVEL OF PARENT IDS: ', levelOfParentIds)
+        if (count === levelOfParentIds) {
+          // Looping through the stack of the preceding htmlElementObjects
+          // by utilizing a decrementing for loop will occur here.
+          // const targetParent = action.payload.parent_id_arr[count - 1]
+          // const htmlElementObj = state.byId[targetParent]
+          // stack.push(htmlElementObj)
+          const reversedStack = reverseArray(stack)
+          const updatedStackObjects = reversedStack.reduce(
+            (acc, curr, index) => {
+              console.log(
+                'THIS IS THE reversedStack OBJECTS ARRAY: ',
+                reversedStack
+              )
+              console.log('THIS IS CURR: ', curr)
+              console.log('THIS IS THE INDEX: ', index)
+              if (index === 0) {
+                acc[index] = {
+                  ...curr,
+                  children_list_byId: {
+                    ...curr.children_list_byId,
+                    [payload.id]: payload,
+                  },
+                  all_children_ids: [...curr.all_children_ids, payload.id],
+                }
+              } else {
+                acc[index] = {
+                  ...curr,
+                  children_list_byId: {
+                    ...curr.children_list_byId,
+                    [acc[index - 1].id]: acc[index - 1],
+                  },
+                  all_children_ids: [...curr.all_children_ids],
+                }
+              }
+              return acc
+            },
+            {}
+          )
+          const topLevelByIdObj = updatedStackObjects[levelOfParentIds - 1]
+          console.log('THE TOP LEVEL BY ID OBJ: ', topLevelByIdObj)
+          return {
+            ...state,
+            byId: {
+              ...state.byId,
+              [topLevelByIdObj.id]: topLevelByIdObj,
+            },
+          }
         }
+        // Do the update to the children_list and all_children_ids
+        // Then begin the unraveling of the stack by utilizing a decrementing
+        // for loop to iterate and update each preceding parent object
+        // and finally return.
+        // const nextParent = parent_id_arr[count - 1]
+        // ********************
+        // LEFT OFF HERE
+        // *********************
+        // AHHH, this is why children list needs to be converted to an object, so that
+        // I may access the child's byId object via this.
+        // I need to rework this. const htmlElementObj = state.byId[firstParent]
+        const nextParent = action.payload.parent_id_arr[count]
+        console.log('THIS IS THE StACK BEFORE ACCESSING CHILD OBJ: ', stack)
+        htmlElementObj = stack[count - 1].children_list_byId[nextParent]
+        console.log(
+          'CHILD HTML ELEMENT OBJ TO BE PUSHED TO STACK: ',
+          htmlElementObj
+        )
+        stack.push(htmlElementObj)
+        count++
         // Otherwise, push htmlElementObj.
         // oh dear... Just as I began to start writing the above sentence. I realized.
         // Maintaining a list of parent objects, for each level of parent will be beneficial
@@ -92,6 +195,14 @@ export default function(state = initialState, action) {
         // against htmlElement.Obj's children_byId. (it's an array now, but this will give me
         // the coveted O(1) look up, that way I can then check the obtained children_byId object
         // for the parent_id...
+        return recurseToFindTargetHtmlById(
+          state,
+          payload,
+          parent_id_arr,
+          count,
+          stack,
+          htmlElementObj
+        )
       }
       return iterateForUpdate(state, action, action.payload.parent_id)
     // return {
